f5471f8d1458a3e0770a6d57bd440b0d2999f2cc
CarlosHenrique191 <carloshr.171@gmail.com>
2023-03-22 11:26:57 -0300

    form editado

--- app-front/pages/CadastrarEsportes.vue
+++ app-front/pages/CadastrarEsportes.vue
@@ -1,66 +1,42 @@
 <template>
     <div id="default-body">
         <b-card bg-variant="light">
-            <b-form-group
-            label-cols-lg="3"
-            label="Shipping Address"
-            label-size="lg"
-            label-class="font-weight-bold pt-0"
-            class="mb-0"
-            >
-            <b-form-group
-                label="Street:"
-                label-for="nested-street"
-                label-cols-sm="3"
-                label-align-sm="right"
-            >
-                <b-form-input id="nested-street"></b-form-input>
-            </b-form-group>
-
-            <b-form-group
-                label="City:"
-                label-for="nested-city"
-                label-cols-sm="3"
-                label-align-sm="right"
-            >
-                <b-form-input id="nested-city"></b-form-input>
-            </b-form-group>
-
-            <b-form-group
-                label="State:"
-                label-for="nested-state"
-                label-cols-sm="3"
-                label-align-sm="right"
-            >
-                <b-form-input id="nested-state"></b-form-input>
-            </b-form-group>
-
-            <b-form-group
-                label="Country:"
-                label-for="nested-country"
-                label-cols-sm="3"
-                label-align-sm="right"
-            >
-                <b-form-input id="nested-country"></b-form-input>
-            </b-form-group>
-
-            <b-form-group
-                label="Ship via:"
-                label-cols-sm="3"
-                label-align-sm="right"
+            <b-form @submit="doCadastro">
+                <b-form-group
+                label-cols-lg="3"
+                label="Cadastro de Esporte"
+                label-size="lg"
+                label-class="font-weight-bold pt-0"
                 class="mb-0"
-                v-slot="{ ariaDescribedby }"
-            >
-                <b-form-radio-group
-                class="pt-2"
-                :options="['Air', 'Courier', 'Mail']"
-                :aria-describedby="ariaDescribedby"
-                ></b-form-radio-group>
-            </b-form-group>
-            </b-form-group>
+                >
+                <b-form-group
+                    label="Nome:"
+                    label-for="nome-input"
+                    label-cols-sm="3"
+                    label-align-sm="right"
+                >
+                    <b-form-input id="nome-input"></b-form-input>
+                </b-form-group>
+                <b-form-group
+                    label="Descrição:"
+                    label-for="desc-input"
+                    label-cols-sm="3"
+                    label-align-sm="right"
+                    description="Descreva sucintamente o esporte que deseja adicionar."
+                >
+                    <b-form-input id="desc-input"></b-form-input>
+                </b-form-group>
+                </b-form-group>
+                <b-button type="submit">Cadastrar Esporte</b-button>
+            </b-form>
         </b-card>
+        
     </div>
 </template>
+
+<script>
+
+</script>
 
 <style>
 #default-body {
@@ -68,4 +44,9 @@
     margin: auto;
     margin-top: 200px;
 }
+
+.cadastro{
+    
+}
 </style>
+
